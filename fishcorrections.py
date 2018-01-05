ea76a86f9255a5fc4c00696fbb908b2c21017c94
prabumukundan <prabumukundan@gmail.com>
2018-01-05 19:50:57 +0530

    Squashed commit of the following:
    commit 06abd6d7ea622004026fc00d2fb167206d6bd86f
    Author: prabumukundan <prabumukundan@gmail.com>
    Date:   Fri Jan 5 19:48:47 2018 +0530
    
        Changes
    
    commit d910ed9eebc8e8788a84581872c6c78fff96a857
    Author: prabumukundan <prabumukundan@gmail.com>
    Date:   Fri Jan 5 19:47:27 2018 +0530
    
        Unit test
    
    commit 6808b95b0b75953ce2caf0c171d0b7c5005fd24f
    Author: prabumukundan <prabumukundan@gmail.com>
    Date:   Fri Jan 5 19:46:15 2018 +0530
    
        Fish Problem
    
    commit 3b4d2ca9f866d02f36b70c56d60f5b7c9e2e1e9a
    Author: prabumukundan <prabumukundan@gmail.com>
    Date:   Tue Jan 2 20:12:25 2018 +0530
    
        fishproblem
    
    commit 49a2edd4c6f68dc98010edcca5887135f494a048
    Author: prabumukundan <prabumukundan@gmail.com>
    Date:   Tue Dec 19 14:43:00 2017 +0530
    
        fishproblem

---
+++ fishcorrections.py
@@ -0,0 +1,118 @@
+import random
+
+class Fish:
+
+    
+    def __init__(self, name, size):
+        self.fish_name = name
+        self.fish_size = size
+
+
+Pike = Fish('Pike', 10)
+Sushi = Fish('Sushi', 12)
+Bubbles = Fish('Bubbles', 8)
+Casper = Fish('Casper', 6)
+Shadow = Fish('Shadow',11 )
+Comet = Fish('Comet', 6)
+Charlie = Fish('Charlie', 11)
+Goldie = Fish('Goldie',1 )
+Nemo = Fish('Nemo',5 )
+Pumpkin = Fish('Pumpkin', 15)
+traut = Fish('traut', 15)
+tuna = Fish('tuna', 15)
+searobin = Fish('searobin', 15)
+whale = Fish('whale', 15)
+tough = Fish('tough', 15)
+spadefish = Fish('spadefish', 15)
+
+class pond:
+
+    
+    pondsname = str(raw_input("enter the pond name:"))
+    
+    def __init__(self, pondsname=None, capacity=0):
+        self.fishlist=[]
+        self.name = pondsname
+        self.capacity = capacity
+        
+    def __addfish__(self, fish):
+        self.fishlist.append((fish.name, fish.size))
+        return self.fishlist
+        
+    def getemptyspace(self):
+        strength = len(self.fishlist)
+        emptyspace = self.capacity - strength
+        print "you have %r emptyspace in this pond" %emptyspace
+        return self.fishlist
+    
+    def fishreduce(self, randomfish):
+        self.fishlist.remove(randomfish)
+        print self.fishlist
+        return self.fishlist
+
+
+    
+naile = pond("naile",10)
+naile.__addfish__(Pike)
+naile.__addfish__(Sushi)
+naile.__addfish__(Bubbles)
+naile.__addfish__(tuna)
+naile.__addfish__(traut)
+naile.__addfish__(searobin)
+
+lotus = pond("lotus",10)
+lotus.__addfish__(Casper)
+lotus.__addfish__(Shadow)
+lotus.__addfish__(Comet)
+lotus.__addfish__(tuna)
+lotus.__addfish__(tough)
+lotus.__addfish__(whale)
+
+brook = pond("brook",10)
+brook.__addfish__(Charlie)
+brook.__addfish__(Goldie)
+brook.__addfish__(Nemo)
+brook.__addfish__(Pumpkin)
+brook.__addfish__(tuna)
+brook.__addfish__(spadefish)
+brook.__addfish__(Pike)
+
+class fisher():
+      
+    def __init__(self):
+        self.selectedfish = []
+        
+    def fisher_details(self):
+        fishername = str(raw_input("enter the fisher name:"))
+    
+    def catchfish(self, userselectedpond):
+        randomfish = random.choice(userselectedpond.fishlist)
+        if randomfish[1] >= 10:
+            self.selectedfish.append(randomfish)
+            print self.selectedfish[0]
+            return  self.selectedfish[0]
+        else:
+            info = "there is no fishes of your expected size"
+            return info
+     
+if __name__== '__main__':
+
+    user = pond()
+    fisher = Fisher()
+    fisher.fisher_details()
+    if user.pondsname == "naile":
+        naile.getemptyspace()
+        random = fisher.catchfish(naile)
+        print random
+        naile.fishreduce(random)
+    elif user.pondsname == "lotus":
+        naile.getemptyspace()
+        randomfish = fisher.catchfish(lotus)
+        lotus.fishreduce(randomfish)
+    elif user.pondsname == "brook":
+        brook.getemptyspace()
+        gotfish = fisher.catchfish(brook)
+        brook.fishreduce(gotfish)
+    else:
+        print "you have entered wrong pond"
+
